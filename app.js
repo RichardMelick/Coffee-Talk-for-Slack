c3b551fe775846131ddc9d0d54505f4f1a822200
Richard Melick <108626576+RichardMelick@users.noreply.github.com>
2025-08-06 21:44:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-  token: process.env.xoxb-9315800654469-9313812699283-GzKm3WKKoRuxFCn89IpN7ofp,
-  appToken: process.env.xapp-1-A0997PUGRBM-9319492280484-ab0c5d36f03af821e7ecf84c9098707c189f4e62f2c8d8b57a723f715853b4d9,
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true
 });
 
