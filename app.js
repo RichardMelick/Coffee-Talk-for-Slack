0e8812bfa07a925efddafae3b19c8bf621f36e79
Richard Melick <108626576+RichardMelick@users.noreply.github.com>
2025-08-06 21:56:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.command('/setup-coffeetalk', async ({ command, ack, say }) => {
   await say(`☕ Setting up Coffee Talk channels for your team...`);
 });
 
+app.command('/ping-coffeetalk', async ({ ack, say }) => {
+  await ack();
+  await say("🏓 Coffee Talk Bot is alive!");
+});
+
 (async () => {
   await app.start();
   console.log('⚡ Coffee Talk Bot is running on Socket Mode');
